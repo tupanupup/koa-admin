f1af2d57d1750f2b837a01897012f2868ac52eca
zyuhan <617307928@qq.com>
2022-02-03 16:38:08 +0800

    feat: 进行admin和role的多对多关联

--- src/model/admin_role_relation.model.js
+++ src/model/admin_role_relation.model.js
@@ -1,22 +1,34 @@
 const { DataTypes } = require('sequelize');
 
 const seq = require('../db/seq');
+const Admin = require('../model/admin.model');
+const Role = require('../model/role.model');
 
 const AdminRoleRelation = seq.define('admin_role_relation', {
   admin_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    comment: '用户id'
+    type: DataTypes.INTEGER,
+    comment: '用户id',
+    references: {
+      model: Admin,
+      key: 'id',
+    },
   },
   role_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    comment: '角色id'
+    type: DataTypes.INTEGER,
+    comment: '角色id',
+    references: {
+      model: Role,
+      key: 'id',
+    },
   }
 }, {
   freezeTableName: true,
   timestamps: false
 });
+
+// 进行admin和role的多对多关联
+Admin.belongsToMany(Role, { through: AdminRoleRelation, foreignKey: 'admin_id', });
+Role.belongsToMany(Admin, { through: AdminRoleRelation, foreignKey: 'role_id', });
 
 // seq.sync({ force: true }).then(() => {
 //   console.log('用户与角色关联模型创建成功！')
