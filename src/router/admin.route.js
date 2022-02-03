a918e310ba945b79f196aef6fc97931a841e2eb3
zyuhan <617307928@qq.com>
2022-02-03 16:38:53 +0800

    feat: 添加获取用户信息接口

--- src/router/admin.route.js
+++ src/router/admin.route.js
@@ -1,8 +1,8 @@
 const Router = require('koa-router');
 
-const { cryptPassword, verifyLogin } = require('../middleware/admin.middleware');
+const { cryptPassword, verifyLogin, verifyToken} = require('../middleware/admin.middleware');
 
-const { register, login } = require('../controller/admin.controller');
+const { register, login, getUserInfo } = require('../controller/admin.controller');
 
 const router = new Router({
   // 添加前缀
@@ -14,5 +14,8 @@ router.post('/register', cryptPassword, register);
 
 // 用户登录
 router.post('/login', verifyLogin, login);
+
+// 获取用户信息
+router.get('/getUserInfo', verifyToken, getUserInfo);
 
 module.exports = router;
