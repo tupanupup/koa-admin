66a6bcbe0245db4216f6a6308faac01b8fbc8859
曾育涵 <617307928@qq.com>
2022-02-18 22:26:30 +0800

    feat: 注释外键关联

--- src/model/admin_role_relation.model.js
+++ src/model/admin_role_relation.model.js
@@ -1,25 +1,23 @@
 const { DataTypes } = require('sequelize');
 
 const seq = require('../db/seq');
-const Admin = require('../model/admin.model');
-const Role = require('../model/role.model');
 
 const AdminRoleRelation = seq.define('admin_role_relation', {
   admin_id: {
     type: DataTypes.INTEGER,
     comment: '用户id',
-    references: {
-      model: Admin,
-      key: 'id',
-    },
+    // references: {
+    //   model: Admin,
+    //   key: 'id',
+    // },
   },
   role_id: {
     type: DataTypes.INTEGER,
     comment: '角色id',
-    references: {
-      model: Role,
-      key: 'id',
-    },
+    // references: {
+    //   model: Role,
+    //   key: 'id',
+    // },
   }
 }, {
   freezeTableName: true,
@@ -27,8 +25,8 @@ const AdminRoleRelation = seq.define('admin_role_relation', {
 });
 
 // 进行admin和role的多对多关联
-Admin.belongsToMany(Role, { through: AdminRoleRelation, foreignKey: 'admin_id', });
-Role.belongsToMany(Admin, { through: AdminRoleRelation, foreignKey: 'role_id', });
+// Admin.belongsToMany(Role, { through: AdminRoleRelation, foreignKey: 'admin_id', });
+// Role.belongsToMany(Admin, { through: AdminRoleRelation, foreignKey: 'role_id', });
 
 // seq.sync({ force: true }).then(() => {
 //   console.log('用户与角色关联模型创建成功！')
