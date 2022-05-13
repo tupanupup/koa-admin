2cd3f7590652d8ebf63b75b97632ee27b34ea2ae
曾育涵 <617307928@qq.com>
2022-05-14 01:50:21 +0800

    feat: 添加修改头像方法

--- src/router/admin.route.js
+++ src/router/admin.route.js
@@ -1,13 +1,15 @@
 const Router = require('koa-router');
 
-const { cryptPassword, verifyLogin, verifyToken} = require('../middleware/admin.middleware');
+const { cryptPassword, verifyLogin, verifyToken } = require('../middleware/admin.middleware');
 
-const { register, login, getUserInfo } = require('../controller/admin.controller');
+const { register, login, getUserInfo, changeAvatar } = require('../controller/admin.controller');
 
 const router = new Router({
   // 添加前缀
   prefix: '/admin'
 });
+
+const upload = require('../upload');
 
 // 用户注册
 router.post('/register', cryptPassword, register);
@@ -17,5 +19,8 @@ router.post('/login', verifyLogin, login);
 
 // 获取用户信息
 router.get('/getUserInfo', verifyToken, getUserInfo);
+
+// 修改头像
+router.post('/changeAvatar', verifyToken, upload.single('file'), changeAvatar);
 
 module.exports = router;
